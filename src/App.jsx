4e8eff4ae20cc0ae6c411fbc3177d2b91a1c11b6
nazifishrak <nzfishrak60@gmail.com>
2024-02-25 17:00:24 -0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import { InputBox } from "./components/index.js";
 
 function App() {
   const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState('usd');
-  const [to, setTo] = useState('bdt');
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("bdt");
   const [convertedAmount, setConvertedAmount] = useState(0);
 
-// Currency info coming from the API through my custom Hook
+  // Currency info coming from the API through my custom Hook
   const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertedAmount(amount * useCurrencyInfo[to]);
+  };
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
   let imgUrl =
     "https://images.pexels.com/photos/534216/pexels-photo-534216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
   return (
@@ -17,7 +30,30 @@ function App() {
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{ backgroundImage: `url(${imgUrl})` }}
     >
-      Hello
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="from"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => {
+                  return setFrom(currency);
+                }}
+                onAmountChange={(amount)=>{
+                  return (setAmount(amount))
+                }}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
